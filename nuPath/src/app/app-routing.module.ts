abe0914247b2fdcab68c528e5a060677566f7171
Sydney S <sydney.silverman3@gmail.com>
2023-03-13 08:18:31 -0400

    updated footer

--- nuPath/src/app/app-routing.module.ts
+++ nuPath/src/app/app-routing.module.ts
@@ -10,10 +10,11 @@ import { NovolandPageComponent } from './novoland-page/novoland-page.component';
 import { NulifePageComponent } from './nulife-page/nulife-page.component';
 import { ProfilePageComponent } from './profile-page/profile-page.component';
 import { SocialsPageComponent } from './socials-page/socials-page.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { DormPageComponent } from './dorm-page/dorm-page.component';
 import { FacilitiesPageComponent } from './facilities-page/facilities-page.component';
 import { PackinglistPageComponent } from './packinglist-page/packinglist-page.component';
+
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
@@ -27,10 +28,11 @@ const routes: Routes = [
   { path: 'academic', component: AcademicPageComponent},
   { path: 'novoland', component: NovolandPageComponent},
   { path: 'profile', component: ProfilePageComponent},
-  { path: '**', component: PageNotFoundComponent },
-  { path: 'dorms', component: DormPageComponent, outlet: 'nulife-content' },
-  { path: 'facilities', component: FacilitiesPageComponent, outlet: 'nulife-content' },
-  { path: 'packinglist', component: PackinglistPageComponent, outlet: 'nulife-content' }
+  { path: 'dorms', component: DormPageComponent },
+  { path: 'facilities', component: FacilitiesPageComponent },
+  { path: 'packinglist', component: PackinglistPageComponent },
+
+  { path: '**', component: PageNotFoundComponent }  //always keep at end
   
 ];
 
