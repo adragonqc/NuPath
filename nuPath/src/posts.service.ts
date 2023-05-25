28ceb7644a6e6f5a4fc1cd99806e8ce6f284850c
Ariella Russin <ariella@russin.us>
2023-05-25 19:30:49 -0400

    needs to be edited but its something

--- nuPath/src/posts.service.ts
+++ nuPath/src/posts.service.ts
@@ -1,9 +1,33 @@
+// Call a method to get the data.
+
 import { Injectable } from '@angular/core';
+import {
+  AngularFirestore,
+  QuerySnapshot
+} from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
-@Injectable({
-  providedIn: 'root'
-})
+import { map } from 'rxjs/operators';
+import {
+  PostResponse,
+} from 'src/app/models/post-response.interface';
+
+@Injectable()
 export class PostsService {
+  constructor(private firestore: AngularFirestore) {}
 
-  constructor() { }
+  getPosts(): Observable<PostResponse[]> {
+    return this.firestore
+      .collection<PostResponse>('posts')
+      .get()
+      .pipe(
+        map((query: QuerySnapshot<PostResponse>) => {
+          return query.docs.map(doc => {
+            const post = doc.data();
+            post.id = doc.id;
+            return post;
+          });
+        })
+      );
+  }
 }
